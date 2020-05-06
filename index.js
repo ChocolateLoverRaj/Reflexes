97e5ffaa8881aeecde605575e71b789f5f3d8ecf
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-05-06 14:55:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/', function (req, res) {
    res.sendFile( __dirname + "/" + "genetics.html" );
 })
 
+app.get('/class', function(req, res){
+   res.sendFile(__dirname + "/" + "class.html");
+});
+
 serv.listen(process.env.PORT);
 console.log("Server is listening on port " + process.env.PORT);
